8114ae674c2f67f0b012458734d0eb086fd2d813
Leonardo <leonardo.bongianni@gmail.com>
2021-03-08 16:16:31 +0100

    Bozza

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+var parola = prompt("Inserisci una parola");
+
+var parolaSplit = split(parola);
+console.log(parolaSplit);
+
+var parolaReverse = reverse(parola);
+console.log(parolaReverse);
+
+var parolaRevJoin = join(parola);
+console.log(parolaRevJoin);
+
+console.log(reverse(parola));
+
+
+// if (reverse(parola) === parolaRevJoin) {
+//   alert("La parola è palindroma");
+// } else
+//   alert("La parola non è palindroma");
+//
+
+// Funzioni
+function split(parola){
+  return parola.split("");
+}
+
+function reverse(parola){
+  return parolaSplit.reverse("");
+}
+
+function join(parola){
+  return parolaReverse.join("");
+}
