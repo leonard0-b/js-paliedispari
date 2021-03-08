ddac790229051a9a260b95f91f9beb118c0b3c60
Leonardo <leonardo.bongianni@gmail.com>
2021-03-08 16:50:50 +0100

    Esercizio Palindromi ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,22 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+// chiedo parola 🐱‍👤
 var parola = prompt("Inserisci una parola").toLowerCase();
 
+// divido i caratteri in un arrey 🐱‍👤
 var parolaSplit = split(parola);
 console.log(parolaSplit);
 
+// faccio il reverse delle lettere 🐱‍👤
 var parolaReverse = reverse(parola);
 console.log(parolaReverse);
 
+// le riunisco in una stringa 🐱‍👤
 var parolaRevJoin = join(parola);
 console.log(parolaRevJoin);
 
-
+// se la parola è uguale alla parola reverse allora è palindroma 🐱‍👤
 if (parola === parolaRevJoin) {
   alert("La parola è palindroma");
 } else
